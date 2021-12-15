6ab292cb4066c36a418a1c72e9a66ef59dfc1591
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2021-12-15 21:26:51 +0700

    ardias

---
+++ HSV_latih_tomat_2.m
@@ -0,0 +1,108 @@
+clc; clear; close all; warning off all;
+
+% % % Proses Latih
+% lokasi data
+nama_folder = 'E:\semester 5\z-projek\progres\Projek-Smt-5\tomat 2\data training';
+% membaca file
+nama_file = dir(fullfile(nama_folder, '*.jpg'));
+% jumlah_file
+jumlah_file = numel(nama_file);
+
+% inilisiasi
+ciri_latih = zeros(jumlah_file,3);
+% mengolah citra
+for n = 1:jumlah_file
+    % membaca file rgb
+    Img = imread(fullfile(nama_folder, nama_file(n).name));
+%     figure, imshow(Img)
+    
+    % rgb to grayscale
+    Img_gray = rgb2gray(Img);
+%     figure, imshow(Img_gray)
+    
+    % grayscale to biner
+    bw = imbinarize(Img_gray);
+%     figure, imshow(bw)
+
+    % operasi komplemen
+    bw = imcomplement(bw);
+%     figure, imshow(bw)
+    
+    % morfologi filling holes
+    bw = imfill(bw,'holes');
+%     figure, imshow(bw)
+    
+    % ekstraksi ciri
+       
+    % ekstrak H S V
+    H = Img(:,:,1);
+    S = Img(:,:,2);
+    V = Img(:,:,3);
+    
+    H(~bw) = 0;
+    S(~bw) = 0;
+    V(~bw) = 0;
+    
+%     figure, imshow(R)
+%     figure, imshow(G)
+%     figure, imshow(B)
+
+    %mengubah background menjadi 0
+    H(~bw) = 0;
+    S(~bw) = 0;
+    V(~bw) = 0;
+    
+%     figure, imshow(H)
+%     figure, imshow(S)
+%     figure, imshow(V)
+    
+    %menghitung rata2 h,s,v
+    Hue = sum(sum(H))/sum(sum(bw));
+    Saturation = sum(sum(S))/sum(sum(bw));
+    Value = sum(sum(V))/sum(sum(bw));
+    
+
+    
+    %menghitung luas objek
+%     Luas = sum(sum(bw));
+       
+    %mengisi variable
+    ciri_latih(n,1) = Hue;
+    ciri_latih(n,2) = Saturation;
+    ciri_latih(n,3) = Value;
+%     ciri_latih(n,4) = Luas;
+end
+
+% menyusun variabel kelas_latih
+kelas_latih = cell(jumlah_file, 1);
+
+% mengisi =ma2 tomat pada variabel kelas_latih
+for k = 1:14
+    kelas_latih{k} = 'Matang';
+end
+
+for k = 15:42
+    kelas_latih{k} = 'Mentah';
+end
+
+for k = 43:81
+    kelas_latih{k} = 'Setengah Matang';
+end
+
+% klasifikasi citra menggunakan algoritma naive bayes
+Mdl = fitcnb(ciri_latih, kelas_latih);
+
+% membaca kelas keluaran hasil pelatihan
+hasil_latih = predict(Mdl,ciri_latih);
+
+% menghitung akurasi latih
+jumlah_benar = 0;
+for k = 1:jumlah_file
+    if isequal(hasil_latih{k}, kelas_latih{k})
+        jumlah_benar = jumlah_benar+1;
+    end
+end
+
+akurasi_latih = jumlah_benar/jumlah_file*100;
+
+save Mdl Mdl
--- HSV_uji_tomat_2.m
+++ HSV_uji_tomat_2.m
@@ -1,89 +1,106 @@
 clc; clear; close all; warning off all;
 
-% % % Proses Latih
+% % % Proses pengujian
 % lokasi data
-nama_folder = 'E:\semester 5\z-projek\data set buah tomat\data uji';
+nama_folder = 'E:\semester 5\z-projek\progres\Projek-Smt-5\tomat 2\data uji';
 % membaca file
 nama_file = dir(fullfile(nama_folder, '*.jpg'));
 % jumlah_file
 jumlah_file = numel(nama_file);
 
 % inilisiasi
-ciri_latih = zeros(jumlah_file,4);
+ciri_uji = zeros(jumlah_file,3);
 % mengolah citra
 for n = 1:jumlah_file
     % membaca file rgb
     Img = imread(fullfile(nama_folder, nama_file(n).name));
 %     figure, imshow(Img)
+    
     % rgb to grayscale
     Img_gray = rgb2gray(Img);
 %     figure, imshow(Img_gray)
+    
     % grayscale to biner
     bw = imbinarize(Img_gray);
 %     figure, imshow(bw)
+
     % operasi komplemen
     bw = imcomplement(bw);
 %     figure, imshow(bw)
+    
     % morfologi filling holes
     bw = imfill(bw,'holes');
 %     figure, imshow(bw)
     
     % ekstraksi ciri
-    %RGB to HSV
-    HSV = rgb2hsv(Img);
-    %figure, imshow(HSV)
-    
+       
     % ekstrak H S V
-    H = HSV(:,:,1);
-    S = HSV(:,:,2);
-    V = HSV(:,:,3);
+    H = Img(:,:,1);
+    S = Img(:,:,2);
+    V = Img(:,:,3);
     
+    H(~bw) = 0;
+    S(~bw) = 0;
+    V(~bw) = 0;
+    
+%     figure, imshow(R)
+%     figure, imshow(G)
+%     figure, imshow(B)
+
     %mengubah background menjadi 0
     H(~bw) = 0;
     S(~bw) = 0;
     V(~bw) = 0;
     
+%     figure, imshow(H)
+%     figure, imshow(S)
+%     figure, imshow(V)
+    
     %menghitung rata2 h,s,v
     Hue = sum(sum(H))/sum(sum(bw));
-    Saturation = sum(sum(H))/sum(sum(bw));
-    Value = sum(sum(H))/sum(sum(bw));
+    Saturation = sum(sum(S))/sum(sum(bw));
+    Value = sum(sum(V))/sum(sum(bw));
+    
+
     
     %menghitung luas objek
-    Luas = sum(sum(bw));
+%     Luas = sum(sum(bw));
        
     %mengisi variable
-    ciri_latih(n,1) = Hue;
-    ciri_latih(n,2) = Saturation;
-    ciri_latih(n,3) = Value;
-    ciri_latih(n,4) = Luas;
+    ciri_uji(n,1) = Hue;
+    ciri_uji(n,2) = Saturation;
+    ciri_uji(n,3) = Value;
+%     ciri_latih(n,4) = Luas;
 end
 
 % menyusun variabel kelas_latih
-kelas_latih = cell(jumlah_file, 1);
+kelas_uji = cell(jumlah_file, 1);
 
 % mengisi =ma2 tomat pada variabel kelas_latih
-for k = 1:35
-    kelas_latih{k} = 'Matang';
+for k = 1:5
+    kelas_uji{k} = 'Setengah Matang';
 end
 
-for k = 36:80
-    kelas_latih{k} = 'Mentah';
+for k = 6:9
+    kelas_uji{k} = 'Mentah';
 end
 
-for k = 81:125
-    kelas_latih{k} = 'Setengah Matang';
+for k = 10:16
+    kelas_uji{k} = 'Matang';
 end
 
 % klasifikasi citra menggunakan algoritma naive bayes
-Mdl = fitcnb(ciri_latih,kelas_latih);
+load Mdl;
 
 % membaca kelas keluaran hasil pelatihan
-hasil_latih = predict(Mdl,ciri_latih);
+hasil_uji = predict(Mdl,ciri_uji);
 
+% menghitung akurasi latih
 jumlah_benar = 0;
-for k =1:jumlah_file
-    if isequal(hasil_latih{k}, kelas_latih{k})
+for k = 1:jumlah_file
+    if isequal(hasil_uji{k}, kelas_uji{k})
         jumlah_benar = jumlah_benar+1;
     end
 end
 
+akurasi_latih = jumlah_benar/jumlah_file*100;
